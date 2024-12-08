42b0f0def27cb5c0edc24a28341c74790b7a6738
Abhimanyu Kumar <abhiroy829429@gmail.com>
2024-12-08 22:27:51 +0530

    youtube

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import Filter from "./Component/filter/Filter";
+import Navbar from "./Component/navbar/Navbar";
+import SideBar from "./Component/sidebar/SideBar";
+import SignUp from "./Component/authorization/SignUp";
+import { useSelector } from "react-redux";
+import Videos from "./Component/Video/Videos";
+
+
+export default function App() {
+  const isSignUpOpen = useSelector((state) => state.ShowSignUpForm);
+  return (
+    <div className="app">
+      <Navbar/>
+      <Filter className="filter" />
+      {isSignUpOpen && <SignUp/>}
+      <SideBar className="sidebar" />
+      <Videos/>
+    </div>
+  );
+}
